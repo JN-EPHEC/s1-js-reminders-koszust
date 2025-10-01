4f895ed41afc0e49d1bab48ea9ba6013bc346b88
Oszust Kacper <oszust.kacper@gmail.com>
2025-10-01 10:07:30 +0200

    Travail Ajouté

--- exercise1.mjs
+++ exercise1.mjs
@@ -8,12 +8,24 @@
 
 // Expected result:
 // [ 'Mobile phone', 'Laptop computer', 'Headphones', 'Smartwatch' ]
+// Your code here
 
-const products = [
-  "  mobile phone ",
-  " laptop computer  ",
-  "Headphones",
-  "  smartwatch",
+const users = [
+  { id: 1, name: "Alice", age: 28, specialty: "Marketing" },
+  { id: 2, name: "Bob", age: 35, specialty: "Engineering" },
+  { id: 3, name: "Charlie", age: 22, specialty: "Marketing" },
+  { id: 4, name: "David", age: 42, specialty: "Engineering" },
+  { id: 5, name: "Eve", age: 29, specialty: "Design" },
 ];
 
-// Your code here
+const userNames = users.map(user => user.name);
+console.log(userNames);
+
+const engineers = users.filter(user => user.specialty === "Engineering");
+console.log(engineers);
+
+//const averageAge = users.length((sum users.length)/users.length);
+//console.log(averageAge);
+
+const userDescriptions = [user.name === A, user.age === B];
+console.log(userDescription.join("A" "is" "B" "years old"));
